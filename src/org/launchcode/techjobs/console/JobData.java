533dce6e556c77742ed7103925b212bcbf77b08a
Oles Ochy <olesochy@oless-air.dhcp.webster.edu>
2018-05-29 19:17:32 -0500

    modified JobData.java case insensitive

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -77,7 +77,8 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+     //       if (aValue.contains(value))
+            if (aValue.toLowerCase().contains(value.toLowerCase())){
                 jobs.add(row);
             }
         }
